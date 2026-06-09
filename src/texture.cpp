b86c2f89c402995aa5050403447cbbf8296e8e44
Lucas Andersen <iwantdeadbydaylight@gmail.com>
2026-06-08 23:58:15 -0700

    began work on dds image loading

--- src/texture.cpp
+++ src/texture.cpp
@@ -1,5 +1,199 @@
 #include "texture.h"
 
+#define BCDEC_IMPLEMENTATION
+#include <bcdec.h>
+
+#include <memory>
+#include <new>
+
+struct DDS_PIXELFORMAT{
+    unsigned int dwSize;
+	unsigned int dwFlags;
+	unsigned int dwFourCC;
+	unsigned int dwRGBBitCount;
+	unsigned int dwRBitMask;
+	unsigned int dwGBitMask;
+	unsigned int dwBBitMask;
+	unsigned int dwABitMask;
+};
+
+struct DDS_HEADER
+{
+	unsigned int dwSize;
+	unsigned int dwFlags;
+	unsigned int dwHeight;
+	unsigned int dwWidth;
+	unsigned int dwPitchOrLinearSize;
+	unsigned int dwDepth;
+	unsigned int dwMipMapCount;
+	unsigned int dwReserved1[11];
+	DDS_PIXELFORMAT ddspf;
+	unsigned int dwCaps;
+	unsigned int dwCaps2;
+	unsigned int dwCaps3;
+	unsigned int dwCaps4;
+	unsigned int dwReserved2;
+};
+
+struct DDS_HEADER_DXT10
+{
+	unsigned int dxgiFormat;
+	unsigned int resourceDimension;
+	unsigned int miscFlag;
+	unsigned int arraySize;
+	unsigned int miscFlags2;
+};
+
+const unsigned int DDSCAPS2_CUBEMAP = 0x200;
+const unsigned int DDSCAPS2_VOLUME = 0x200000;
+
+const unsigned int DDS_DIMENSION_TEXTURE2D = 3;
+
+enum DXGI_FORMAT
+{
+	DXGI_FORMAT_BC1_UNORM = 71,
+	DXGI_FORMAT_BC1_UNORM_SRGB = 72,
+	DXGI_FORMAT_BC2_UNORM = 74,
+	DXGI_FORMAT_BC2_UNORM_SRGB = 75,
+	DXGI_FORMAT_BC3_UNORM = 77,
+	DXGI_FORMAT_BC3_UNORM_SRGB = 78,
+	DXGI_FORMAT_BC4_UNORM = 80,
+	DXGI_FORMAT_BC4_SNORM = 81,
+	DXGI_FORMAT_BC5_UNORM = 83,
+	DXGI_FORMAT_BC5_SNORM = 84,
+	DXGI_FORMAT_BC6H_UF16 = 95,
+	DXGI_FORMAT_BC6H_SF16 = 96,
+	DXGI_FORMAT_BC7_UNORM = 98,
+	DXGI_FORMAT_BC7_UNORM_SRGB = 99,
+};
+
+static unsigned int fourCC(const char (&str)[5])
+{
+	return (unsigned(str[0]) << 0) | (unsigned(str[1]) << 8) | (unsigned(str[2]) << 16) | (unsigned(str[3]) << 24);
+}
+
+static VkFormat getFormat(const DDS_HEADER& header, const DDS_HEADER_DXT10& header10)
+{
+	if (header.ddspf.dwFourCC == fourCC("DXT1"))
+		return VK_FORMAT_BC1_RGBA_UNORM_BLOCK;
+	if (header.ddspf.dwFourCC == fourCC("DXT3"))
+		return VK_FORMAT_BC2_UNORM_BLOCK;
+	if (header.ddspf.dwFourCC == fourCC("DXT5"))
+		return VK_FORMAT_BC3_UNORM_BLOCK;
+	if (header.ddspf.dwFourCC == fourCC("ATI1"))
+		return VK_FORMAT_BC4_UNORM_BLOCK;
+	if (header.ddspf.dwFourCC == fourCC("ATI2"))
+		return VK_FORMAT_BC5_UNORM_BLOCK;
+
+	if (header.ddspf.dwFourCC == fourCC("DX10"))
+	{
+		switch (header10.dxgiFormat)
+		{
+		case DXGI_FORMAT_BC1_UNORM:
+		case DXGI_FORMAT_BC1_UNORM_SRGB:
+			return VK_FORMAT_BC1_RGBA_UNORM_BLOCK;
+		case DXGI_FORMAT_BC2_UNORM:
+		case DXGI_FORMAT_BC2_UNORM_SRGB:
+			return VK_FORMAT_BC2_UNORM_BLOCK;
+		case DXGI_FORMAT_BC3_UNORM:
+		case DXGI_FORMAT_BC3_UNORM_SRGB:
+			return VK_FORMAT_BC3_UNORM_BLOCK;
+		case DXGI_FORMAT_BC4_UNORM:
+			return VK_FORMAT_BC4_UNORM_BLOCK;
+		case DXGI_FORMAT_BC4_SNORM:
+			return VK_FORMAT_BC4_SNORM_BLOCK;
+		case DXGI_FORMAT_BC5_UNORM:
+			return VK_FORMAT_BC5_UNORM_BLOCK;
+		case DXGI_FORMAT_BC5_SNORM:
+			return VK_FORMAT_BC5_SNORM_BLOCK;
+		case DXGI_FORMAT_BC6H_UF16:
+			return VK_FORMAT_BC6H_UFLOAT_BLOCK;
+		case DXGI_FORMAT_BC6H_SF16:
+			return VK_FORMAT_BC6H_SFLOAT_BLOCK;
+		case DXGI_FORMAT_BC7_UNORM:
+		case DXGI_FORMAT_BC7_UNORM_SRGB:
+			return VK_FORMAT_BC7_UNORM_BLOCK;
+		}
+	}
+
+	return VK_FORMAT_UNDEFINED;
+}
+
+static size_t getImageSizeBC(unsigned int width, unsigned int height, unsigned int levels, unsigned int blockSize)
+{
+	size_t result = 0;
+
+	for (unsigned int i = 0; i < levels; ++i)
+	{
+		result += ((width + 3) / 4) * ((height + 3) / 4) * blockSize;
+
+		width = width > 1 ? width / 2 : 1;
+		height = height > 1 ? height / 2 : 1;
+	}
+
+	return result;
+}
+
+static size_t getImageSizeRGBA(unsigned int width, unsigned int height, unsigned int levels)
+{
+	size_t result = 0;
+
+	for (unsigned int i = 0; i < levels; ++i)
+	{
+		result += size_t(width) * size_t(height) * 4;
+
+		width = width > 1 ? width / 2 : 1;
+		height = height > 1 ? height / 2 : 1;
+	}
+
+	return result;
+}
+
+bool createDDSImage(Image& image, VkDevice device, VmaAllocator allocator, VkCommandPool commandPool,
+    VkCommandBuffer commandBuffer, VkQueue queue, const Buffer& staging, const char* path){
+    FILE* file = nullptr;
+    errno_t err = fopen_s(&file, path, "rb");
+
+    if(err != 0 || !file){
+        printf("Failed to open file: %s\n Error Code: %d\n",path,err);
+        return false;
+    }
+
+    std::unique_ptr<FILE,int (*)(FILE*)> filePtr(file,fclose);
+
+    unsigned int magic = 0;
+    if(fread_s(&magic,sizeof(magic),sizeof(size_t),1,file) != 1 || magic != fourCC("DDS ")){
+        return false;
+    }
+
+    DDS_HEADER header = {};
+    if(fread_s(&header,sizeof(header),sizeof(size_t),1,file)!=1){
+        return false;
+    }
+
+    DDS_HEADER_DXT10 header10 = {};
+    if(header.ddspf.dwFourCC == fourCC("DX10")&&fread_s(&header10,sizeof(header10),sizeof(size_t),1,file)!=1){
+        return false;
+    }
+
+    if(header.dwSize != sizeof(header) || header.ddspf.dwSize != sizeof(header.ddspf)){
+        return false;
+    }
+
+    if(header.dwCaps2 & (DDSCAPS2_CUBEMAP | DDSCAPS2_VOLUME)){
+        return false;
+    }
+
+    if(header.ddspf.dwFourCC == fourCC("DX10") && header10.resourceDimension != DDS_DIMENSION_TEXTURE2D){
+        return false;
+    }
+
+    VkFormat format = getFormat(header,header10);
+    if(format == VK_FORMAT_UNDEFINED){
+        return false;
+    }
+}
+
 
 
 Image createKTXImage(VmaAllocator allocator,VkDevice device, VkQueue queue,
--- src/texture.h
+++ src/texture.h
@@ -7,5 +7,5 @@
 Image createKTXImage(VmaAllocator allocator,VkDevice device, VkQueue queue,
     VkCommandBuffer commandBuffer, ktxTexture2* texture, VkImageUsageFlags usage);
 
-Image createDDSImage(Image& image, VkDevice device, VmaAllocator allocator, VkCommandPool commandPool,
+bool createDDSImage(Image& image, VkDevice device, VmaAllocator allocator, VkCommandPool commandPool,
     VkCommandBuffer commandBuffer, VkQueue queue, const Buffer& staging, const char* path);
